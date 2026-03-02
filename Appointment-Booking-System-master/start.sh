fc1aaa5e454f3ebd39ff602870299d80f2267179
hameedshaik2345 <hameedshaik786.01@gmail.com>
2026-03-02 23:59:03 +0530

    Fix file permissions and package discovery for container

--- Appointment-Booking-System-master/start.sh
+++ Appointment-Booking-System-master/start.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+
 # Create SQLite Database if it doesn't exist
 touch database/database.sqlite
+touch /var/www/html/.env
+
+# Discover packages since composer install was run without scripts
+php artisan package:discover --ansi
 
 # Run Laravel migrations and seed database
 php artisan migrate --seed --force
+
+# Fix permissions for web server (since migrations run as root)
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/database
+
+# Clear cache and optimize
+php artisan optimize:clear
 
 # Start Apache process in the foreground
 apache2-foreground
